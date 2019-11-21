5badaf6cb32eca31cf235fcbafcf66d621929662
AustinOlig <austinoligario@gmail.com>
2019-11-20 23:01:25 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
-<script>
-  var music = document.getElementById("bg-music");
-  var mute = document.getElementById("mute-button");
+var music = document.getElementById("bg-music");
+var mute = document.getElementById("mute-button");
 
-  music.volume = 0.1;
-  music.muted = false;
+music.volume = 0.1;
+music.muted = false;
 
-  mute.onclick = function toggleMusic() {
-    if (music.muted == false) {
-        music.muted = true;
-    } else {      
-        music.muted = false;
-    }
+mute.onclick = function toggleMusic() {
+  if (music.muted == false) {
+      music.muted = true;
+  } else {      
+      music.muted = false;
   }
-</script> 
+}
