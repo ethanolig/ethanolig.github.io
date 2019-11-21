19294bfe4f78346fe315c1faa56ed0a2ef6993c4
AustinOlig <austinoligario@gmail.com>
2019-11-20 22:20:43 -0500

    created script to toggle music

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+<script>
+var music = document.getElementById("music");
+function toggleMusic() {
+  if (music.muted == false) {
+      music.muted = true;
+  } else {      
+      music.muted = false;
+  }
+}
+</script> 
