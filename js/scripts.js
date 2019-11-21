1c40bfd52536f91d9972b6fa8d094f5a6a47557c
AustinOlig <austinoligario@gmail.com>
2019-11-20 23:30:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
 var music = document.getElementById("bg-music");
-var mute = document.getElementById("mute-button");
 
 music.volume = 0.25;
 music.muted = false;
 
-//mute.onclick =   y
 function toggleMusic() {
   if (document.getElementById("bg-music").muted == false) {
       document.getElementById("bg-music").muted = true;
